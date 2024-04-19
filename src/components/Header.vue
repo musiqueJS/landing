d587a3615894ec3bd2e72f24a5d81cd3a6d15b2a
Nicolas de Garrigues <ndegarrigues@gmail.com>
2024-04-19 16:31:17 +0200

    release(1.2.0): releasing JSONPartition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
 <template>
 	<div class="announcement">
 		<span>🎉</span>
-		<a href="https://github.com/musiqueJS/musiqueJS/releases/tag/v1.1.0" target="_blank">
+		<a href="https://github.com/musiqueJS/musiqueJS/releases/tag/v1.2.0" target="_blank">
 			Musique.js v1.1.0 is released!
 		</a>
 		<span>🎉</span>
